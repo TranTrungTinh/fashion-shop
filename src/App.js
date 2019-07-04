82e0e2f58b61e1667825cc493484aa413ebcfadd
Tran Trung Tinh <trantrungtinh4954@gmail.com>
2019-07-04 22:07:30 +0700

    done shop, signin component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 
-class App extends Component {
-  render() {
-    return (
-    <div className='App'>
-      <HomePage />
-    </div>
-    );
-  }
-}
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+
+
+const App = () => (
+  <div>
+    <Header />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
+    </Switch>
+  </div>
+);
 
 export default App;
